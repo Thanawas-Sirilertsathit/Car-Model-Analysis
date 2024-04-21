132ca48f3d08f9f3d0aae7af90590f2e0bb6fdfc
Thanawas-Sirilertsathit <thanawas.s@ku.th>
2024-04-21 21:07:51 +0700

    Added docstring
    added docstring for CarDescribe file

--- car_describe.py
+++ car_describe.py
@@ -2,6 +2,7 @@ from car import Car
 import pandas as pd
 
 def describe():
+    """Descriptive statistics for horsepower, stroke, city mpg and highway mpg"""
     c = Car()
     desc = c.df[["horsepower","stroke","citympg","highwaympg"]]
     print(desc.describe())
